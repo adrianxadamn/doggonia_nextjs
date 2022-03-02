d30fb90a0be76d2d949f60b03cc49ce836aac61d
Adrian Nuyda <adamnuyda@yahoo.com>
2022-03-02 13:12:28 -0800

    adds contact form

--- pages/api/contact-us/index.js
+++ pages/api/contact-us/index.js
@@ -1,5 +1,31 @@
-// contact-us
+// /api/contact-us
+import fs from 'fs';
+import path from 'path';
+
+export function buildFeedbackPath() {
+  return path.join(process.cwd(), 'data', 'contact-submissions.json');
+}
+
+export function extractFeedback(filePath) {
+  const fileData = fs.readFileSync(filePath);
+  const data = JSON.parse(fileData);
+  return data;
+}
 
 export default async function handler(req, res) {
-  res.status(200).json({ message: 'hi'});
+
+  const contactSubmission = {
+    id: new Date().toISOString(),
+    name: req.body.name,
+    phoneNumber: req.body.phoneNumber,
+    email: req.body.email,
+    message: req.body.message,
+  }
+
+  // store that in a database or in a file
+  const filePath = buildFeedbackPath();
+  const data = extractFeedback(filePath);
+  data.push(contactSubmission);
+  fs.writeFileSync(filePath, JSON.stringify(data));
+  res.status(201).json({ message: 'Success!', data: contactSubmission });
 }
--- pages/pages/contact-us.js
+++ pages/pages/contact-us.js
@@ -1,9 +1,69 @@
-import React from 'react';
+import React, { useRef } from 'react';
+import { getPage } from '@/lib/shopify';
+import Button from '@/components/button';
+const ContactUsPage = ({pageData}) => {
 
-const ContactUsPage = ({props}) => {
+  const nameRef = useRef();
+  const emailRef = useRef();
+  const phoneRef = useRef();
+  const messageRef = useRef();
+  
+  const submitForm = async (e) => {
+    e.preventDefault();
+
+    fetch('/api/contact-us', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        name: nameRef.current.value,
+        email: emailRef.current.value,
+        phone: phoneRef.current.value,
+        message: messageRef.current.value,
+      })
+    })
+    .then(res => res.json())
+    .then((data) => {
+      console.log(data);
+    });
+  }
+
   return (
-
+    <div className="basicPage">
+      <h1>{pageData.title}</h1>
+      <form onSubmit={(e) => submitForm(e)} className="form">
+        <div className="input-group--wrapper">
+          <div className="input-group">
+            <input className="input" name="contact[name]" type="text" placeholder="Name" ref={nameRef} />
+          </div>
+          <div className="input-group">
+            <input className="input" name="contact[email]" type="email" placeholder="Email" ref={emailRef} />
+          </div>
+        </div>
+        <div className="input-group">
+          <input className="input" name="contact[phone]" type="phone" placeholder="Phone Number" ref={phoneRef} />
+        </div>
+        <div className="input-group">
+          <textarea className="textarea" name="contact[body]" placeholder="Message" ref={messageRef} />
+        </div>
+        <div className="form-submit-btn">
+          <Button>Send</Button>
+        </div>
+      </form>
+    </div>
   );
 };
+
+export async function getStaticProps() {
+  const pageData = await getPage('contact-us');
+  return {
+    props: {
+      pageData,
+      handle: pageData.handle
+    },
+  }
+}
+
 
 export default ContactUsPage;
