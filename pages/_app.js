897a84331479a2730213c3cb7a3e41d65e61cce3
Adrian Nuyda <adamnuyda@yahoo.com>
2022-03-02 17:58:07 -0800

    test css modules workaround

--- pages/_app.js
+++ pages/_app.js
@@ -4,8 +4,28 @@ config.autoAddCss = false; // Tell Font Awesome to skip adding the CSS automatic
 
 import '../styles/globals.scss';
 import Layout from '../components/layout';
+import Router from "next/router";
+
+const routeChange = () => {
+  // Temporary fix to avoid flash of unstyled content
+  // during route transitions. Keep an eye on this
+  // issue and remove this code when resolved:
+  // https://github.com/vercel/next.js/issues/17464
+
+  const tempFix = () => {
+    const allStyleElems = document.querySelectorAll('style[media="x"]');
+    allStyleElems.forEach((elem) => {
+      elem.removeAttribute("media");
+    });
+  };
+  tempFix();
+};
+
+Router.events.on("routeChangeComplete", routeChange );
+Router.events.on("routeChangeStart", routeChange );
 
 function MyApp({ Component, pageProps }) {
+  
   return (
     <Layout>
       <Component {...pageProps} />
